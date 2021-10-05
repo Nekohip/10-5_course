b54346053d2d16acb0fbdda538c84ee9af9a3324
Nao <shangyuchan@gmail.com>
2021-10-05 16:04:54 +0800

    add guess_num

---
+++ guess_num.py
@@ -0,0 +1,30 @@
+import random
+
+zone1 = int(input("輸入最小值"))
+zone2 = int(input("輸入最大值"))
+x =  random.randint(zone1,zone2)
+#print(x)
+for i in range(5):
+    n=5
+    bingo = False
+    while True:
+        try:
+            m=n-i
+            num = int(input(f"猜數字--{zone1}~{zone2}，還有{m}次機會: "))
+            if num == x :
+                print("你猜對了，我的朋友!")
+                bingo = True
+                break
+            if num > x :
+                print("猜小一點")
+                zone2 = num-1
+            else:
+                print("猜大一點")
+                zone1 = num+1
+            break
+        except Exception as e:
+	        print(e)
+    if bingo == True :
+        break
+print(f"結束!答案是{x}")
+    
